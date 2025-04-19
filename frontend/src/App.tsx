b109ee224b8531311b3f1179e72937e5ae6262b0
Stefan Schmidt <schmidt@beontra.com>
2025-04-20 00:00:15 +0200

    LX-13: Mature frontend version

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
+import CustomerProvider from "./state/CustomerProvider.tsx";
+import CustomerList from "./components/CustomerList.tsx";
+
 const App = () => (
-    <>
-    </>
+    <CustomerProvider>
+        <CustomerList />
+    </CustomerProvider>
 );
 
 export default App
